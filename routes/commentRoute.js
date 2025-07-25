2764d8ffc3007dd9f0d106f125588179c369bd4b
Rehnuma111 <rehnumabano1111@gmail.com>
2025-07-25 15:21:01 +0530

    fix invalid signatjre issue

--- routes/commentRoute.js
+++ routes/commentRoute.js
@@ -1,23 +1,24 @@
 import express from "express";
 const commentRouter = express.Router();
-import { addComment, getComment, likeTheComment, deleteComment, editComment, getAllComments } from "../controller/commentController.js";
+import {
+  addComment,
+  getComment,
+  likeTheComment,
+  deleteComment,
+  editComment,
+  getAllComments,
+} from "../controller/commentController.js";
 import verifyUserMiddleware from "../middleware/verifyUserMiddleware.js";
 
-
-
-
-
+import dotenv from "dotenv";
+dotenv.config();
 
 commentRouter
-    .post('/add-comment', verifyUserMiddleware, addComment)
-    .get('/get-comment/:blogId', getComment)
-    .put('/like-the-comment/:commentId', verifyUserMiddleware, likeTheComment)
-    .delete('/delete-comment/:commentId', verifyUserMiddleware, deleteComment)
-    .put('/edit-comment/:commentId', verifyUserMiddleware, editComment)
-    .get('/get-all-comments', verifyUserMiddleware, getAllComments)
+  .post("/add-comment", verifyUserMiddleware, addComment)
+  .get("/get-comment/:blogId", getComment)
+  .put("/like-the-comment/:commentId", verifyUserMiddleware, likeTheComment)
+  .delete("/delete-comment/:commentId", verifyUserMiddleware, deleteComment)
+  .put("/edit-comment/:commentId", verifyUserMiddleware, editComment)
+  .get("/get-all-comments", verifyUserMiddleware, getAllComments);
 
-
-
-
-
-export default commentRouter; 
+export default commentRouter;
